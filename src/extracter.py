2402573c1b87f6a7efd660f70004ed5939d6cc79
Mazars Lab <mazars.lab@gmail.com>
2016-08-11 15:25:26 +0200

    Init extracter

---
+++ src/extracter.py
@@ -0,0 +1,16 @@
+class Extracter(object):
+
+    def __init__(self):
+        return None
+
+    def applyOCR(self):
+        return None
+
+    def html(self):
+        return None
+
+    def txt(self):
+        return None
+
+    def csv(self):
+        return None
